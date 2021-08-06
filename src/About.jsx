1afc1a7701d0a683075d1a3d5cd930588fdd2a33
sunil singh <singhsunilwadwal@gmail.com>
2021-08-06 19:44:31 +0530

    changes the about.jsx

--- src/About.jsx
+++ src/About.jsx
@@ -5,7 +5,7 @@ import Common from './Common'
 const About = () => {
     return (
         <>
-            <Common name="Welcome to about page " visit="/Contact" contentname = " We are team to servive" imgsrc={web} />
+            <Common name="Welcome to about page " visit="/Contact" contentname = " We are team to servive and provide the knowledgble content" imgsrc={web} />
         </>
     );
 };
